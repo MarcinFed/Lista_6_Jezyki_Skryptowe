a1c9eafcd3e9b532efec54aa46b414e70f1b68c1
MarcinFed <fedorowicz.marcin02@gmail.com>
2023-04-18 19:38:28 +0200

    Poprawki

--- lab_1.py
+++ lab_1.py
@@ -1,22 +1,51 @@
 from ipaddress import IPv4Address
 from abc import ABC, abstractmethod
 import re
-from tools import parse_log_line, TIMESTAMP, HOST_NAME, MESSAGE, PID_NUMBER, IPV4_PATTERN
+import datetime
+
+TIMESTAMP = "timestamp"
+HOST_NAME = "host_name"
+PID_NUMBER = "pid_number"
+MESSAGE = "message"
+TIMESTAMP_MATCH = 1
+HOST_NAME_MATCH = 2
+PID_NUMBER_MATCH = 4
+MESSAGE_MATCH = 5
+LOG_PATTERN = r"^(\w{3}\s\d{1,2}\s\d{2}:\d{2}:\d{2})\s(\S+)\s(\S+)\[(\d+)\]:\s(.*)$"
+IPV4_PATTERN = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+TIMESTAMP_FORMAT = "%b %d %H:%M:%S"
+TYPE_ERROR_MESSAGE = "Trying to compare non SSHLogEntry object"
+MATCHING_EXCEPTION_MESSAGE = "No match in log line!"
+
+
+def parse_log_line(log):
+    try:
+        match = re.match(LOG_PATTERN, log)
+        if not match:
+            return {}
+        return {
+            TIMESTAMP: datetime.datetime.strptime(match.group(TIMESTAMP_MATCH), TIMESTAMP_FORMAT),
+            HOST_NAME: match.group(HOST_NAME_MATCH),
+            PID_NUMBER: match.group(PID_NUMBER_MATCH),
+            MESSAGE: match.group(MESSAGE_MATCH),
+        }
+    except Exception:
+        raise Exception(MATCHING_EXCEPTION_MESSAGE)
 
 
 class SSHLogEntry(ABC):
     def __init__(self, log):
         prepared_log = parse_log_line(log)
         self.timestamp = prepared_log[TIMESTAMP]
-        self.__message = prepared_log[MESSAGE]
+        self._message = prepared_log[MESSAGE]
         self.pid_number = prepared_log[PID_NUMBER]
         self.host_name = prepared_log[HOST_NAME] if prepared_log[HOST_NAME] else None
 
     def __str__(self):
-        return f"[{self.timestamp}] [{self.pid_number}] [{self.host_name}] [{self.__message}]" if self.host_name else f"[{self.timestamp}] [{self.pid_number}] [{self.__message}]"
+        return f"[{self.timestamp}] [{self.pid_number}] [{self.host_name}] [{self._message}]" if self.host_name else f"[{self.timestamp}] [{self.pid_number}] [{self._message}]"
 
     def get_ipv4_address(self):
-        match = re.search(IPV4_PATTERN, self.__message)
+        match = re.search(IPV4_PATTERN, self._message)
         return IPv4Address(match.group(0)) if match else None
 
     @abstractmethod
@@ -28,20 +57,20 @@ class SSHLogEntry(ABC):
         return self.get_ipv4_address() is not None
 
     def __repr__(self):
-        return f"SSHLogEntry (timestamp={self.timestamp}, host_name={self.host_name}, pid_number={self.pid_number}, message={self.__message})"
+        return f"SSHLogEntry (timestamp={self.timestamp}, host_name={self.host_name}, pid_number={self.pid_number}, message={self._message})"
 
     def __eq__(self, other):
         if not isinstance(other, SSHLogEntry):
-            raise TypeError("Trying to compare non SSHLogEntry object")
-        return (self.timestamp, self.host_name, self.pid_number, self.__message) == (other.timestamp, other.host_name, other.pid_number, other.__message)
+            raise TypeError(TYPE_ERROR_MESSAGE)
+        return (self.timestamp, self.host_name, self.pid_number, self._message) == (other.timestamp, other.host_name, other.pid_number, other._message)
 
     def __lt__(self, other):
         if not isinstance(other, SSHLogEntry):
-            raise TypeError("Trying to compare non SSHLogEntry object")
+            raise TypeError(TYPE_ERROR_MESSAGE)
         return self.timestamp < other.timestamp
 
     def __gt__(self, other):
         if not isinstance(other, SSHLogEntry):
-            raise TypeError("Trying to compare non SSHLogEntry object")
+            raise TypeError(TYPE_ERROR_MESSAGE)
         return self.timestamp > other.timestamp
 
--- lab_2.py
+++ lab_2.py
@@ -1,38 +1,57 @@
 from lab_1 import SSHLogEntry
-from tools import get_user_name, get_error_msg
+import re
+
+FAILED_PASSWORD = "Failed password for invalid user"
+ACCEPTED_PASSWORD = "Accepted password"
+FOR = "for"
+FROM = "from"
+RECEIVED_DISCONNECT = "Received disconnect from"
+USER_NAME_PATTERN = r"(?<=user )\w+"
+ERROR_PATTERN = r"error: (.+?) \["
+
+
+def get_user_name(message):
+    try:
+        return re.findall(USER_NAME_PATTERN, message)[0]
+    except Exception:
+        raise IndexError("No user_name in message")
+
+
+def get_error_msg(message):
+    return re.findall(ERROR_PATTERN, message)[0]
 
 
 class RejectedPasswordSSHLogEntry(SSHLogEntry):
     def __init__(self, log):
         super().__init__(log)
-        self.user_name = get_user_name(self._SSHLogEntry__message)
+        self.user_name = get_user_name(self._message)
 
     def validate(self):
-        return "Failed password for invalid user" in self._SSHLogEntry__message
+        return FAILED_PASSWORD in self._message
 
 
 class AcceptedPasswordSSHLogEntry(SSHLogEntry):
     def __init__(self, log):
         super().__init__(log)
-        self.user_name = get_user_name(self._SSHLogEntry__message)
+        self.user_name = get_user_name(self._message)
 
     def validate(self):
-        return "Accepted password" and "for" and "from" in self._SSHLogEntry__message
+        return ACCEPTED_PASSWORD and FOR and FROM in self._message
 
 
 class ErrorSSHLogEntry(SSHLogEntry):
     def __init__(self, log):
         super().__init__(log)
-        self.error_message = get_error_msg(self._SSHLogEntry__message)
+        self.error_message = get_error_msg(self._message)
 
     def validate(self):
-        return "Received disconnect from" in self._SSHLogEntry__message
+        return RECEIVED_DISCONNECT in self._message
 
 
 class OtherSSHLogEntry(SSHLogEntry):
     def __init__(self, log):
         super().__init__(log)
-        self.other_message = self._SSHLogEntry__message
+        self.other_message = self._message
 
     def validate(self):
         return True
